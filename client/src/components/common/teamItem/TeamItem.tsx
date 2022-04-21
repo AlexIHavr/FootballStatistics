de45ea2d126279be3069013f7ba56e0fba5cd151
AlexIHavr <dchavronin@bk.ru>
2022-04-21 12:59:42 +0300

    create favorites page

--- client/src/components/common/teamItem/TeamItem.tsx
+++ client/src/components/common/teamItem/TeamItem.tsx
@@ -1,11 +1,24 @@
 import { Avatar, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { urls } from '../../../constants/app';
+import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
+import { toggleFavoritesTeam } from '../../../redux/leagueTeams/reducer';
 import { TeamItemProps } from '../../../types/props';
 
 import './teamItem.scss';
 
 const TeamItem: React.FC<TeamItemProps> = ({ id, name, shortName, crestUrl, tabControl }) => {
+  const dispatch = useAppDispatch();
+  const { favoritesTeams } = useAppSelector((state) => state.leagueTeams);
+
+  const toggleFavoritesTeamOnClick = useCallback(
+    (id: number) => {
+      dispatch(toggleFavoritesTeam(id));
+    },
+    [dispatch]
+  );
+
   return (
     <ListItem className="teamItem">
       <ListItemAvatar>
@@ -17,7 +30,15 @@ const TeamItem: React.FC<TeamItemProps> = ({ id, name, shortName, crestUrl, tabC
           <>
             <span>shortName: {shortName}</span>
             <br />
-            <a className="addToFavorites waves-effect btn">Add team to favorites</a>
+
+            <a
+              className="addToFavorites waves-effect btn"
+              onClick={() => toggleFavoritesTeamOnClick(id)}
+            >
+              {!favoritesTeams.find((teamId) => teamId === id)
+                ? 'Add to favorites'
+                : ' Remove from favorites'}
+            </a>
           </>
         }
       />
--- client/src/redux/leagueTeams/reducer.ts
+++ client/src/redux/leagueTeams/reducer.ts
@@ -4,17 +4,26 @@ import { InitialState, LeagueTeams } from './types';
 
 const initialState: InitialState = {
   leagueTeamsData: {},
+  favoritesTeams: [],
 };
 
 export const leagueTeamsSlice = createSlice({
   name: 'leagueTeams',
   initialState,
-  reducers: {},
+  reducers: {
+    toggleFavoritesTeam: (state, action: PayloadAction<number>) => {
+      state.favoritesTeams.find((id) => id === action.payload)
+        ? (state.favoritesTeams = state.favoritesTeams.filter((id) => id !== action.payload))
+        : state.favoritesTeams.push(action.payload);
+    },
+  },
   extraReducers: {
     [setLeagueTeamsData.fulfilled.type]: (state, action: PayloadAction<LeagueTeams>) => {
       state.leagueTeamsData[action.payload.shortName] = action.payload.teams;
     },
   },
 });
+
+export const { toggleFavoritesTeam } = leagueTeamsSlice.actions;
 
 export default leagueTeamsSlice.reducer;
